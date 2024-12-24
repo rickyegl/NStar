79e8278394cca66c1f8aba86bc35ce5fb563e274
rickyegl <135090025+rickyegl@users.noreply.github.com>
2024-12-23 23:40:12 -0600

    Update localinstall.sh

--- localinstall.sh
+++ localinstall.sh
@@ -21,30 +21,4 @@ pip3 install --find-links https://tortall.net/~robotpy/wheels/2023/raspbian robo
 # Install Pillow with specific flags
 LDFLAGS="-L/usr/lib/aarch64-linux-gnu" CFLAGS="-I/usr/include/aarch64-linux-gnu" pip3 install -v pillow
 
-# Download OpenCV and OpenCV contrib
-wget -O opencv.tar.gz https://github.com/opencv/opencv/archive/refs/tags/4.6.0.tar.gz
-wget -O opencv_contrib.tar.gz https://github.com/opencv/opencv_contrib/archive/refs/tags/4.6.0.tar.gz
-
-# Extract OpenCV and OpenCV contrib
-tar -xvzf opencv.tar.gz
-tar -xvzf opencv_contrib.tar.gz
-
-# Build and install OpenCV
-cd opencv-4.6.0
-mkdir build
-cd build
-
-cmake -D CMAKE_BUILD_TYPE=Release \
-      -D CMAKE_INSTALL_PREFIX=/usr/local \
-      -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.6.0/modules \
-      -D ENABLE_NEON=ON \
-      -D ENABLE_VFPV3=ON \
-      -D BUILD_TESTS=OFF \
-      -D OPENCV_ENABLE_NONFREE=ON \
-      -D INSTALL_C_EXAMPLES=OFF \
-      -D INSTALL_PYTHON_EXAMPLES=OFF \
-      -D BUILD_EXAMPLES=OFF ..
-
-make -j$(nproc)
-make install
-ldconfig
+sh installOpenCV.sh
