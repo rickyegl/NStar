3b437fe53f14a2ba3a1a2ea0e246d52fec71dff9
rickyegl <135090025+rickyegl@users.noreply.github.com>
2024-12-24 00:26:31 -0600

    Update config.py

--- northstar/config/config.py
+++ northstar/config/config.py
@@ -5,23 +5,23 @@ import numpy.typing
 
 @dataclass
 class LocalConfig:
-    device_id: str = ""
-    server_ip: str = ""
+    device_id: str = "xenic"
+    server_ip: str = "10.66.47.2"
     stream_port: int = 8000
-    has_calibration: bool = False
+    has_calibration: bool = True
     camera_matrix: numpy.typing.NDArray[numpy.float64] = numpy.array([])
     distortion_coefficients: numpy.typing.NDArray[numpy.float64] = numpy.array([])
 
 
 @dataclass
 class RemoteConfig:
-    camera_id: str = ""
-    camera_resolution_width: int = 0
-    camera_resolution_height: int = 0
-    camera_auto_exposure: int = 0
-    camera_exposure: int = 0
-    camera_gain: int = 0
-    fiducial_size_m: float = 0
+    camera_id: str = "0"
+    camera_resolution_width: int = 1600
+    camera_resolution_height: int = 1200
+    camera_auto_exposure: int = 1
+    camera_exposure: int = 1
+    camera_gain: int = 25
+    fiducial_size_m: float = 0.1651
     tag_layout: any = None
 
 
